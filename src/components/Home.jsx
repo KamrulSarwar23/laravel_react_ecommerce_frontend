17c453bb02a2e20c5a6e1475fcd0454f60020dd9
Kamrul Hasan <kamrul939223@gmail.com>
2025-01-10 18:28:12 +0600

    Home & Shop Page

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <header className='shadow'>
+        <div className='bg-dark text-center py-3'>
+            <span className='text-white'>Your Fashion Partner</span>
+        </div>
+    </header>
+  )
+}
